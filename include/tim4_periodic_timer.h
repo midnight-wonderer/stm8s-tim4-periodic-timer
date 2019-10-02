80c1225075222d87ea35cc52365d41042c46c26d
Sarun Rattanasiri <midnight_w@gmx.tw>
2019-10-03 02:07:30 +0700

    modify file content for the new name

--- include/tim4_periodic_timer.h
+++ include/tim4_periodic_timer.h
@@ -1,25 +1,25 @@
 // Copyright (c) 2018 Sarun Rattanasiri
 // under the MIT License https://opensource.org/licenses/MIT
 
-#ifndef __TIM4_TICK_H
-#define __TIM4_TICK_H
+#ifndef __TIM4_PERIODIC_TIMER_H
+#define __TIM4_PERIODIC_TIMER_H
 
 #include <stdint.h>
 #include "stm8s.h"
 #include "stm8s_itc.h"
 #include "stm8s_tim4.h"
 
-typedef struct _tim4_tick__config {
+typedef struct _tim4_periodic_timer__config {
   TIM4_Prescaler_TypeDef prescaler;
   uint8_t reload_value;
-  void (*tick)();
-} Tim4Tick_Config;
+  void (*ticked)();
+} Tim4PeriodicTimer_Config;
 
 // example config parameters for 2 MHz PSC
 // prescaler = TIM4_PRESCALER_16
 // reload_value = (0x100 - 125)
 
-void tim4_tick__setup(Tim4Tick_Config* config);
-INTERRUPT_HANDLER(tim4_tick__interrupt_routine, ITC_IRQ_TIM4_OVF);
+void tim4_periodic_timer__setup(Tim4PeriodicTimer_Config* config);
+INTERRUPT_HANDLER(tim4_periodic_timer__tick, ITC_IRQ_TIM4_OVF);
 
 #endif
